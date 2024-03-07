c80ac9b4bae629c29a0351e665eb186560b6d580
zhnuradil <zhakenger.n@ryadom.kz>
2024-03-07 22:10:33 +0500

    add resources

--- DeliveryApp/app/src/main/java/com/kiparo/deliveryapp/presentation/core_ui/theme/Font.kt
+++ DeliveryApp/app/src/main/java/com/kiparo/deliveryapp/presentation/core_ui/theme/Font.kt
@@ -4,7 +4,16 @@
 
 package com.kiparo.deliveryapp.presentation.core_ui.theme
 
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.kiparo.deliveryapp.R
+
 // For Students, read this:
 // https://developer.android.com/jetpack/compose/text/fonts
 
-// TODO Implement
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_medium, FontWeight.Medium)
+)
